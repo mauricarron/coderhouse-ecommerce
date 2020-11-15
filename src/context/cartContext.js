05305505f2624293e7fb81e9338b430e8c44bc14
mdCarron <mdcarron.dev@gmail.com>
2020-11-15 20:40:49 -0300

    completa desafio

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -1,19 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
 const CartContext = React.createContext([]);
+export const useCartContext = () => useContext(CartContext);
 
-function CartProvider({ children }) {
+export default function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
 
-  // addItem
+  function addItem(item) {
+    let sameItem = cart.find((same) => same.item.id === item.item.id);
 
-  // removeItem
+    if (!sameItem) {
+      setCart([...cart, item]);
+    } else {
+      const updatedItem = {
+        ...sameItem,
+        quantity: sameItem.quantity + item.quantity,
+      };
 
-  // clear
+      let index = cart.indexOf(item.id);
+      cart.splice(index, 1, updatedItem);
+
+      setCart([...cart]);
+    }
+  }
+
+  function removeItem(itemId) {
+    setCart([...cart.filter((cartItem) => cartItem.item.itemId !== itemId)]);
+  }
+
+  function clear() {
+    setCart([]);
+  }
 
   return (
-    <CartContext.Provider value={{ cart }}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ cart, addItem, removeItem, clear }}>
+      {children}
+    </CartContext.Provider>
   );
 }
-
-export default CartProvider;
